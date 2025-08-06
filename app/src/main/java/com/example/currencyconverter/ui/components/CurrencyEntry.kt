9fc881441401c0e9d655ddda2e6c9ed6a3a0d72e
Vefery <54257165+Vefery@users.noreply.github.com>
2025-08-06 13:43:45 +0700

    Moved flags to local res, made balance always visible

--- app/src/main/java/com/example/currencyconverter/ui/components/CurrencyEntry.kt
+++ app/src/main/java/com/example/currencyconverter/ui/components/CurrencyEntry.kt
@@ -1,5 +1,6 @@
 package com.example.currencyconverter.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -29,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -37,6 +39,42 @@ import coil.decode.SvgDecoder
 import coil.request.ImageRequest
 import com.example.currencyconverter.R
 import com.example.currencyconverter.data.dataSource.remote.dto.RateDto
+
+private val flags: Map<String, Int> = mapOf(
+    "EUR" to R.drawable.eur,
+    "AUD" to R.drawable.aud,
+    "BGN" to R.drawable.bgn,
+    "BRL" to R.drawable.brl,
+    "CAD" to R.drawable.cad,
+    "CHF" to R.drawable.chf,
+    "CNY" to R.drawable.cny,
+    "CZK" to R.drawable.czk,
+    "DKK" to R.drawable.dkk,
+    "GBP" to R.drawable.gbp,
+    "HKD" to R.drawable.hkd,
+    "HRK" to R.drawable.hrk,
+    "HUF" to R.drawable.huf,
+    "IDR" to R.drawable.idr,
+    "ILS" to R.drawable.ils,
+    "INR" to R.drawable.inr,
+    "ISK" to R.drawable.isk,
+    "JPY" to R.drawable.jpy,
+    "KRW" to R.drawable.krw,
+    "MXN" to R.drawable.mxn,
+    "MYR" to R.drawable.myr,
+    "NOK" to R.drawable.nok,
+    "NZD" to R.drawable.nzd,
+    "PHP" to R.drawable.php,
+    "PLN" to R.drawable.pln,
+    "RON" to R.drawable.ron,
+    "RUB" to R.drawable.rub,
+    "SEK" to R.drawable.sek,
+    "SGD" to R.drawable.sgd,
+    "THB" to R.drawable.thb,
+    "TRY" to R.drawable.flag_try,
+    "USD" to R.drawable.usd,
+    "ZAR" to R.drawable.zar
+)
 
 @Composable
 fun CurrencyEntry(
@@ -70,19 +108,24 @@ fun CurrencyEntry(
             modifier = Modifier.padding(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            SubcomposeAsyncImage(
+//            SubcomposeAsyncImage(
+//                modifier = Modifier.padding(5.dp).size(70.dp),
+//                model = ImageRequest.Builder(LocalContext.current)
+//                    .data("https://www.xe.com/svgs/flags/${rate.currency.lowercase()}.static.svg")
+//                    .decoderFactory(SvgDecoder.Factory())
+//                    .build(),
+//                contentDescription = null,
+//                loading = {
+//                    CircularProgressIndicator(modifier = Modifier.requiredSize(40.dp), strokeWidth = 2.dp)
+//                },
+//                error = {
+//                    Icon(Icons.Default.Warning, contentDescription = null)
+//                }
+//            )
+            Image(
                 modifier = Modifier.padding(5.dp).size(70.dp),
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data("https://www.xe.com/svgs/flags/${rate.currency.lowercase()}.static.svg")
-                    .decoderFactory(SvgDecoder.Factory())
-                    .build(),
-                contentDescription = null,
-                loading = {
-                    CircularProgressIndicator(modifier = Modifier.requiredSize(40.dp), strokeWidth = 2.dp)
-                },
-                error = {
-                    Icon(Icons.Default.Warning, contentDescription = null)
-                }
+                painter = painterResource(flags[rate.currency.uppercase()] ?: R.drawable.notfound),
+                contentDescription = null
             )
             Column(modifier = Modifier.weight(1f)) {
                 Text(
@@ -94,12 +137,10 @@ fun CurrencyEntry(
                     text = currencyName,
                     style = MaterialTheme.typography.bodyMedium
                 )
-                if (balance > 0.0) {
-                    Text(
-                        text = "${stringResource(R.string.balance)}: ${balanceFormatter(balance)}",
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
+                Text(
+                    text = "${stringResource(R.string.balance)}: ${balanceFormatter(balance)}",
+                    style = MaterialTheme.typography.bodySmall
+                )
             }
             if (isEditing) {
                 CurrencyInput(
